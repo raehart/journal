c0c8456beb95b3f19977b7fe156f91759e141199
Brigette Eckert <brigette.eckert@live.com>
2016-06-14 01:57:51 -0700

    added templates for entries an enteries details

--- journal/views.py
+++ journal/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from journal.models import Entry
 
 
 def entries_list(request):
-    return render(request, 'journal/entries_list.html')
+    entries = Entry.objects.all()
+    return render(request, 'journal/entries_list.html', {'entries': entries})
 
 
-def entry_detail(request):
-    return render(request, 'journal/entry_detail.html')
+def entry_detail(request, pk):
+    entry = get_object_or_404(Entry, pk=pk)
+    return render(request, 'journal/entry_detail.html', {'entry': entry})
