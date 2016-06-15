e3d082f1e8fa748508a948791e38fea99223a6ca
Brigette Eckert <brigette.eckert@live.com>
2016-06-14 23:22:34 -0700

    replaced skills text with icons with title

--- journal/models.py
+++ journal/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Skill(models.Model):
     name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
